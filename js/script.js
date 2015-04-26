cac22989d0de86be77ab6db5b17d7fba22fb838a
DavidYourNeighbor <dnaber2@illinois.edu>
2015-04-26 20:35:08 +0000

    Very very basic file structure for project

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-var divs = document.getElementsByTagName('div');
+/*var divs = document.getElementsByTagName('div');
 for(var i=0; i<divs.length; i++) {
   divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
+  
+  //divs[i].addEventListener("click", highlightThis, true);
+  //divs[i].addEventListener("click", highlightThis, false);
 }
 
 function highlightThis(event) {
@@ -13,4 +13,4 @@ function highlightThis(event) {
     this.style.backgroundColor='yellow';
     alert(this.className);
     this.style.backgroundColor=backgroundColor;
-}
+}*/
